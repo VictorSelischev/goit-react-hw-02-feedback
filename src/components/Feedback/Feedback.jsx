aa82f6067a83686a28854c5c1da211f0e2468776
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-10-04 14:58:14 +0300

    Привязывает defaultProps компонента Feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import css from './Feedback.module.css';
 
 class Feedback extends Component {
-  state = {
+  static defaultProps = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
+  state = {
+    good: this.props.good,
+    neutral: this.props.neutral,
+    bad: this.props.bad,
+  };
+
   render() {
     console.log(css);
+
+    const { good, neutral, bad } = this.props;
     return (
       <div className={css.section}>
         <h2 className={css.section__title}>Пожалуйста, оставьте отзыв</h2>
@@ -26,9 +34,9 @@ class Feedback extends Component {
         </ul>
         <h2 className={css.section__title}>Statistics</h2>
         <ul>
-          <li className={css.section__listItem}>Good: 3</li>
-          <li className={css.section__listItem}>Neutral: 2</li>
-          <li className={css.section__listItem}>Bad: 2</li>
+          <li className={css.section__listItem}>Good: {good}</li>
+          <li className={css.section__listItem}>Neutral: {neutral}</li>
+          <li className={css.section__listItem}>Bad: {bad}</li>
         </ul>
       </div>
     );
